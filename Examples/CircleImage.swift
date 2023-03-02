16a1a4565e945c78cfc5bd956aa82120a1452f4f
lin <dglmarty@gmail.com>
2023-03-02 21:50:43 +0800

    feat: Creating and Combining Views

---
+++ Examples/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Examples
+//
+//  Created by Lin on 2023/3/2.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
